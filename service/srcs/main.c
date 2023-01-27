b221e6be55af6e0653b89267dfd2b8b15ea4992f
tde-nico <101901057+tde-nico@users.noreply.github.com>
2023-01-27 21:11:27 +0100

    better_service

--- service/srcs/main.c
+++ service/srcs/main.c
@@ -3,26 +3,6 @@
 // https://social.msdn.microsoft.com/Forums/vstudio/en-US/89a4a707-778f-4e64-96d3-ca4a6c89f709/linking-error-lnk2019?forum=vclanguage
 #pragma comment(lib, "advapi32.lib")
 
-void	install(void)
-{
-	printf("install\n");
-}
-
-void	start(void)
-{
-	printf("start\n");
-}
-
-void	stop(void)
-{
-	printf("stop\n");
-}
-
-void	delete(void)
-{
-	printf("delete\n");
-}
-
 void	get_error(char *msg)
 {
 	DWORD	errorMessageID = GetLastError();
@@ -39,10 +19,67 @@ void	get_error(char *msg)
 	LocalFree(messageBuffer);
 }
 
+void	install(SC_HANDLE manager)
+{
+	char		path[MAX_PATH];
+	SC_HANDLE	service;
+
+	GetModuleFileName(NULL, path, MAX_PATH);
+	service = CreateService(manager, NAME, NAME, SERVICE_ALL_ACCESS,
+		SERVICE_WIN32_OWN_PROCESS, SERVICE_DEMAND_START, SERVICE_ERROR_NORMAL,
+		path, NULL, NULL, NULL, NULL, NULL);
+	if (!service)
+	{
+		get_error("Error: Installation: ");
+		return ;
+	}
+	CloseServiceHandle(service);
+	printf("Service <%s> installed successfully\n", NAME);
+}
+
+void	start(SC_HANDLE manager)
+{
+	CloseServiceHandle(manager);
+	printf("Service <%s> started successfully\n", NAME);
+}
+
+void	stop(SC_HANDLE manager)
+{
+	CloseServiceHandle(manager);
+	printf("Service <%s> stopped successfully\n", NAME);
+}
+
+void	delete(SC_HANDLE manager)
+{
+	SC_HANDLE		service;
+	SERVICE_STATUS	status;
+
+	service = OpenService(manager, NAME, SERVICE_ALL_ACCESS);
+	if (!service)
+	{
+		get_error("Error: Deleting: ");
+		return ;
+	}
+	QueryServiceStatus(service, &status);
+	if (status.dwCurrentState != SERVICE_STOPPED)
+	{
+		CloseServiceHandle(service);
+		printf("Service <%s> running, please stop\n", NAME);
+		return ;
+	}
+	if (!DeleteService(service))
+	{
+		get_error("Error: DeleteService: ");
+		return ;
+	}
+	CloseServiceHandle(service);
+	printf("Service <%s> deleted successfully\n", NAME);
+}
+
 int	main(int argc, char **argv)
 {
 	int			i;
-	void		(*operations[4])(void) = { install, start, stop, delete };
+	void		(*operations[4])(SC_HANDLE) = { install, start, stop, delete };
 	char		*str_operations[4] = { "install", "start", "stop", "delete" };
 	SC_HANDLE	manager;
 
@@ -61,7 +98,7 @@ int	main(int argc, char **argv)
 	while (++i < 4)
 	{
 		if (!strcmp(str_operations[i], argv[1]))
-			operations[i]();
+			operations[i](manager);
 	}
 	CloseServiceHandle(manager);
 	return (0);
